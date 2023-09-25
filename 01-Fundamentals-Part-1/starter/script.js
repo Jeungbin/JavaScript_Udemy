b6187cd906f88462a95c598b0fe71986049f03b5
jeongbin <qls2467@gmail.com>
2023-09-25 17:54:01 -0600

    'First'

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,12 @@
+// let js = "amazing";
+// if (js === "amazing") alert("JS is fun");
+
+// console.log("hello");
+// console.log(typeof true);
+
+let markMass = 78;
+let markHeight = 169;
+let johnMass = 92;
+let johnHeight = 195;
+
+let markHigherBMI =  > ;
---
+++ 03-Developer-Skills/starter/script.js
@@ -0,0 +1,8 @@
+// Remember, we're gonna use strict mode in all scripts now!
+'use strict';
+
+const x = 23;
+
+const year = birth => 2015 - birth;
+
+console.log(6);
